61ae84d404073d03169ec1eef6a3d1fed823b9ab
Mateusz Swieton <mateusz.swieton@gmail.com>
2021-08-12 15:59:28 +0200

    Add first file

---
+++ Basics.js
@@ -0,0 +1,36 @@
+// BASICS:
+
+/* 
+Multi-line commment
+*/
+
+// Single-line comment
+
+// Variables
+var text1 = "Hello World";
+var text2 = "!";
+var textFinal = text1 + text2;
+
+let variable1 = "Zmienna";
+const CONST_AMMOUNT = "Stała";
+
+// `backtick can make string literals
+// or multiline variables`
+
+// Naming
+var output, _variable1, $test;
+var camelCaseIsNiceAndFunny;
+
+// Print to console
+console.log(FinalText);
+
+// Tablice - Listy
+
+var tab = [3, "tekst", 99, 12];
+tab[1] = 4; // change value of x element
+console.log(tab[1]); // Print second element
+
+// ***** TO DO *****
+// JSON
+// JQuery
+// Promises
